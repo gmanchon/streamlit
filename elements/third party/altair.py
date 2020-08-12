9b502080c46e674b836b9c00454d224d2616cda6
Gaetan Manchon <gmanchon@gmail.com>
2020-08-12 10:29:58 +0200

    cache added for altair and plotly data generation functions

--- elements/third party/altair.py
+++ elements/third party/altair.py
@@ -7,6 +7,14 @@ import numpy as np
 def title():
     return 'Altair, Vega Lite, Bokeh, pydeck, Deck GL, Graphviz'
 
+@st.cache
+def get_data():
+    print('get_data altair called')
+    return pd.DataFrame(
+            np.random.randn(200, 3),
+            columns=['a', 'b', 'c']
+        )
+
 def run():
 
     st.write('Here is a code sample for Altair, please refer to the [streamlit API reference](https://docs.streamlit.io/en/stable/api.html#display-charts) for other code samples')
@@ -14,9 +22,7 @@ def run():
     with st.echo():
         import altair as alt
 
-        df = pd.DataFrame(
-            np.random.randn(200, 3),
-            columns=['a', 'b', 'c'])
+        df = get_data()
 
         c = alt.Chart(df).mark_circle().encode(
             x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
--- elements/third party/plotly.py
+++ elements/third party/plotly.py
@@ -4,15 +4,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+@st.cache
+def get_data():
+    print('get_data plotly called')
+    z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+    z = z_data.values
+    sh_0, sh_1 = z.shape
+    x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
+    return x, y, z
+
 def run():
 
     with st.echo():
         import plotly.graph_objects as go
 
-        z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
-        z = z_data.values
-        sh_0, sh_1 = z.shape
-        x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
+        x, y, z = get_data()
+
         fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
         fig.update_layout(title='IRR', autosize=False, width=800, height=800, margin=dict(l=40, r=40, b=40, t=40))
         st.plotly_chart(fig)
