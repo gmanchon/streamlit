4c91cd45aa9409d33798fb8165fcb0c77bc9387e
Gaetan Manchon <gmanchon@gmail.com>
2020-08-13 07:59:13 +0200

    placeholder comments modified

--- elements/layout/placeholder.py
+++ elements/layout/placeholder.py
@@ -10,11 +10,11 @@ def run():
     st.markdown('`write`cannot be used on placeholders, use `markdown` instead')
 
     with st.echo():
-        # this defines the location where the placeholder will be displayed
+        # this defines the location where the placeholded code will be displayed
         placeholder = st.empty()
 
-        # this should be displayed after, according to its position in the code,
-        # but gets displayed before
+        # this should be displayed before, according to its position in the code,
+        # but gets displayed after because it is comes after the placeholder
         st.write('2. Some other element')
 
         placeholder.markdown('1. This gets displayed before even though it is called later')
