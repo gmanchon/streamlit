12de7fd7a0499b75c92b00be5a1a95d2d6484325
Gaetan Manchon <gmanchon@gmail.com>
2020-08-15 15:51:41 +0200

    link added to file list colorization streamlit app

--- elements/other/inspiration.py
+++ elements/other/inspiration.py
@@ -5,4 +5,5 @@ def run():
 
     st.markdown("""
         * [Awesome site and nice multipage navigation in sidebar using radio buttons](https://awesome-streamlit.org/)
+        * [Tiny app allowing to add comments and colors to a file list returned by a tree command](https://colorize-file-list.herokuapp.com/)
     """)
