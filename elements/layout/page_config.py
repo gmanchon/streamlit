9aa36318bee23e175320759c612fe509d3ea7bf3
Gaetan Manchon <gmanchon@gmail.com>
2020-08-26 20:07:10 +0200

    changed page config description

--- elements/layout/page_config.py
+++ elements/layout/page_config.py
@@ -8,7 +8,7 @@ def run():
 
     st.write('🚧 Page configuration is a beta feature and should evolve soon 🚧')
 
-    st.write('Page configuration allows to configure the favicon and title of the page in the browser tab')
+    st.write('Page configuration allows to configure the favicon and title of the page in the browser tab, as well as the initial state of the sidebar and the general layout of the page')
 
     st.write('⚠️ `beta_set_page_config` should be the first streamlit statement of the app ⚠️')
 
