da607777324d1a75312daf124c0add6b1f95c5eb
Gaetan Manchon <gmanchon@gmail.com>
2021-06-03 16:39:23 +0200

    add password protection code sample

---
+++ elements/other/password.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+
+def title():
+    return 'Password 🔑'
+
+def run():
+
+    st.write('## Protect your app with a password')
+
+    st.write('A simple solution to protect your app without having to manage a database or user accounts is to use a password. In order to do so, you need to load the password from an environment variable and match it to the user input')
+
+    st.write('Here is a [demo app](https://wagon-streamlit-auth.herokuapp.com/) and the [code of the repo](https://github.com/gmanchon/streamlit_auth)')
