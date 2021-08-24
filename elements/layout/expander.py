5ecde5e087c35bb59a070ceef6019de5fcbc9f02
Gaetan Manchon <gmanchon@gmail.com>
2021-08-24 18:14:44 +0200

    add expander code sample

---
+++ elements/layout/expander.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+def run():
+
+    st.write('Expander allows to hide optional controls')
+
+    with st.echo():
+
+        expander = st.expander("Optional controls")
+
+        expander.radio("Day of week", ["Monday", "Tuesday", "Wednesday"])
