221f205ce19d36b8e0372cff3480bf733e43d1e3
Gaetan Manchon <gmanchon@gmail.com>
2021-08-25 16:54:18 +0200

    update session state code sample

--- elements/session state/variables.py
+++ elements/session state/variables.py
@@ -14,6 +14,7 @@ def run():
     with st.echo():
 
         unpersisted_variable = 0  # here is how a normal variable behaves for comparison
+
         if "persisted_variable" not in st.session_state:
             st.session_state.persisted_variable = 0  # initialize the session state variable
 
@@ -29,6 +30,6 @@ def run():
 
         st.write(st.session_state)
 
-        st.write(f"Unpersistent variable: {unpersisted_variable} 👈 the value does not take into account previous page reruns")
-        st.write(f"Persisted variable: {st.session_state.persisted_variable} 👈 the data is persisted between reruns")
+        st.write(f"Unpersistent variable: {unpersisted_variable} 👈 the value does not take into account previous page reruns ⛔️")
+        st.write(f"Persisted variable: {st.session_state.persisted_variable} 👈 the data is persisted between reruns 👌")
         st.write(f'Persisted variable: {st.session_state["persisted_variable"]}')
