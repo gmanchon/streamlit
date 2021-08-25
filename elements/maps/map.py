6d4169c47170d31d52b51ba37d9aee598ac357e4
Gaetan Manchon <gmanchon@gmail.com>
2021-08-25 12:54:28 +0200

    simplify map sample code

--- elements/maps/map.py
+++ elements/maps/map.py
@@ -9,17 +9,12 @@ def run():
     with st.echo():
         @st.cache
         def get_map_data():
-            print('get_map_data called')
+
             return pd.DataFrame(
                     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
                     columns=['lat', 'lon']
                 )
 
-        if st.checkbox('Show map', False):
-            df = get_map_data()
+        df = get_map_data()
 
-            st.map(df)
-        else:
-            from PIL import Image
-            image = Image.open('images/map.png')
-            st.image(image, caption='map', use_column_width=False)
+        st.map(df)
